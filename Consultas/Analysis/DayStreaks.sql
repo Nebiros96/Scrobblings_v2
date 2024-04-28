d10e093c2707ad4896812d27b46ede8c602da2e2
Julian <juliangomez9614@gmail.com>
2024-04-27 19:39:44 -0500

    nuevo analisis y actualización del stored procedure

---
+++ Consultas/Analysis/DayStreaks.sql
@@ -0,0 +1,28 @@
+-- Rachas consecutivas de días escuchando música
+WITH LagColumn AS (
+    SELECT 
+        Year_Month_Day,
+        COUNT(*) AS Listens,
+        LAG(Year_Month_Day, 1) OVER (ORDER BY Year_Month_Day ASC) AS PreviousDay 
+    FROM Scrobblings_fix
+    GROUP BY Year_Month_Day
+),
+Agrupacion AS (
+    SELECT
+        Year_Month_Day,
+        Listens,
+        PreviousDay,
+        DATEDIFF(DAY, PreviousDay, Year_Month_Day) AS DayDiff,
+        SUM(CASE WHEN DATEDIFF(DAY, PreviousDay, Year_Month_Day) != 1 THEN 1 ELSE 0 END) OVER (ORDER BY Year_Month_Day) AS Bandera
+    FROM LagColumn
+)
+SELECT 
+    MIN(Year_Month_Day) AS StartDate,
+    MAX(Year_Month_Day) AS EndDate,
+    COUNT(*) AS StreakDays,
+	SUM(Listens) AS Listens,
+	SUM(Listens)/COUNT(*) AS ListensPerDay
+FROM Agrupacion
+GROUP BY Bandera
+HAVING COUNT(*) > 1
+ORDER BY StreakDays DESC, Listens DESC;
