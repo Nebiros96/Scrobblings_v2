614eab084b72e1265994a73190eeaf33d6a0dd4c
Julian <juliangomez9614@gmail.com>
2024-12-13 19:50:16 -0500

    actualizacion al 13 de diciembre de 2024

--- Consultas/Analysis/ScroblingsDayHour.sql
+++ Consultas/Analysis/ScroblingsDayHour.sql
@@ -5,6 +5,7 @@ SELECT
 	COUNT(*) AS Scrobblings,
 	ROUND(CAST(COUNT(*)AS FLOAT)/CAST(COUNT(DISTINCT(Year_Month_Day))AS DECIMAL(16,2)),2) AS rep_per_day
 FROM Clean_LastfmData
+-- WHERE Year = '2024'
 GROUP BY 
 	WeekDay, Hour
 ORDER BY 
